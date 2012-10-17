270e953b7a5d6b8713c223f1ba2dd577ea303ad6
Thomas Løcke <thomas@12boo.net>
2012-10-17 10:38:53 +0200

    Refactoring. Replace declare blocks with procedures.

--- demo/src/view-email.adb
+++ demo/src/view-email.adb
@@ -29,6 +29,18 @@ with Yolk.Log;
 
 package body View.Email is
 
+   procedure Populate_Form
+     (Address : in     String;
+      Name    : in     String;
+      T       :    out AWS.Templates.Translate_Set);
+   --  Insert the form data associations into the T translate set.
+
+   procedure Send_Email
+     (Address : in     String;
+      Name    : in     String;
+      T       :    out AWS.Templates.Translate_Set);
+   --  Construct and send an email to recipient.
+
    ---------------
    --  Generate --
    ---------------
@@ -38,100 +50,26 @@ package body View.Email is
       return AWS.Response.Data
    is
       use AWS.Templates;
-      use Yolk.Log;
+      use Ada.Strings;
 
-      procedure Populate_Form
-        (T             :    out Translate_Set;
-         Recip_Name    : in     String;
-         Recip_Address : in     String);
-      --  Insert the form data associations into the T translate set.
-
-      ---------------------
-      --  Populate_Form  --
-      ---------------------
-
-      procedure Populate_Form
-        (T             :    out Translate_Set;
-         Recip_Name    : in     String;
-         Recip_Address : in     String)
-      is
-      begin
-         Insert (T, Assoc ("RECIP_NAME", Recip_Name));
-         Insert (T, Assoc ("RECIP_ADDRESS", Recip_Address));
-         Insert
-           (T, Assoc ("SMTP_HOST", String'(My.Config.Get (My.SMTP_Host))));
-         Insert
-           (T, Assoc ("SMTP_PORT", String'(My.Config.Get (My.SMTP_Port))));
-      end Populate_Form;
-
-      P : constant AWS.Parameters.List := AWS.Status.Parameters (Request);
-      T : Translate_Set;
+      P       : constant AWS.Parameters.List :=
+                  AWS.Status.Parameters (Request);
+      Name    : constant String :=
+                  Fixed.Trim (P.Get ("recip_name"), Both);
+      Address : constant String :=
+                  Fixed.Trim (P.Get ("recip_address"), Both);
+      T       : Translate_Set;
    begin
-      if P.Exist ("recip_name")
-        and then P.Exist ("recip_address")
+      if Name'Length > 0
+        and then Address'Length > 0
       then
-         declare
-            use Ada.Strings;
-            use Yolk.Email;
-
-            P_Recip_Name      : constant String :=
-                                  Fixed.Trim (P.Get ("recip_name"), Both);
-            P_Recip_Address   : constant String :=
-                                  Fixed.Trim (P.Get ("recip_address"), Both);
-            Email             : Structure;
-         begin
-            if P_Recip_Address /= "" then
-               Composer.Add_Custom_Header (ES      => Email,
-                                           Name    => "User-Agent",
-                                           Value   => "Yolk " & Yolk.Version);
-               Composer.Send (ES           => Email,
-                              From_Address => "thomas@12boo.net",
-                              From_Name    => "Thomas Løcke",
-                              To_Address   => P_Recip_Address,
-                              To_Name      => P_Recip_Name,
-                              Subject      => "Test email",
-                              Text_Part    => "Test email from Yolk",
-                              SMTP_Server  => My.Config.Get (My.SMTP_Host),
-                              SMTP_Port    => My.Config.Get (My.SMTP_Port),
-                              Charset      => UTF8);
-
-               if Composer.Is_Send (Email) then
-                  Insert (T, Assoc ("IS_SEND", True));
-                  Insert (T, Assoc ("SMTP_HOST",
-                    String'(My.Config.Get (My.SMTP_Host))));
-
-                  Trace (Handle  => Info,
-                         Message => "Email sent to " &
-                         P_Recip_Address &
-                         " using " &
-                         String'(My.Config.Get (My.SMTP_Host)));
-               else
-                  Insert (T, Assoc ("IS_SEND", False));
-                  --  Sending failed.
-
-                  Trace (Handle  => Error,
-                         Message => "Email failed to " &
-                         P_Recip_Address &
-                         " using " &
-                         String'(My.Config.Get (My.SMTP_Host)));
-
-                  Populate_Form (T             => T,
-                                 Recip_Name    => P_Recip_Name,
-                                 Recip_Address => P_Recip_Address);
-               end if;
-            else
-               Insert (T, Assoc ("IS_SEND", False));
-               --  No recipient address, so obviously we cannot send the email.
-
-               Populate_Form (T             => T,
-                              Recip_Name    => P_Recip_Name,
-                              Recip_Address => P_Recip_Address);
-            end if;
-         end;
+         Send_Email (Address => Address,
+                     Name    => Name,
+                     T                 => T);
       else
-         Populate_Form (T             => T,
-                        Recip_Name    => "Zaphod Beeblebrox",
-                        Recip_Address => "yolk@mailinator.com");
+         Populate_Form (Address => "yolk@mailinator.com",
+                        Name    => "Zaphod Beeblebrox",
+                        T                 => T);
       end if;
 
       return Build_Response
@@ -139,5 +77,88 @@ package body View.Email is
          Template_File => My.Config.Get (My.Template_Email),
          Translations  => T);
    end Generate;
+
+   ---------------------
+   --  Populate_Form  --
+   ---------------------
+
+   procedure Populate_Form
+     (Address : in     String;
+      Name    : in     String;
+      T       :    out AWS.Templates.Translate_Set)
+   is
+      use AWS.Templates;
+   begin
+      Insert (T, Assoc ("RECIP_NAME", Name));
+      Insert (T, Assoc ("RECIP_ADDRESS", Address));
+      Insert
+        (T, Assoc ("SMTP_HOST", String'(My.Config.Get (My.SMTP_Host))));
+      Insert
+        (T, Assoc ("SMTP_PORT", String'(My.Config.Get (My.SMTP_Port))));
+   end Populate_Form;
+
+   ------------------
+   --  Send_Email  --
+   ------------------
+
+   procedure Send_Email
+     (Address : in     String;
+      Name    : in     String;
+      T       :    out AWS.Templates.Translate_Set)
+   is
+      use AWS.Templates;
+      use Yolk.Email;
+      use Yolk.Log;
+
+      Email : Structure;
+   begin
+      if Address /= "" then
+         Composer.Add_Custom_Header (ES      => Email,
+                                     Name    => "User-Agent",
+                                     Value   => "Yolk " & Yolk.Version);
+         Composer.Send (ES           => Email,
+                        From_Address => "thomas@12boo.net",
+                        From_Name    => "Thomas Løcke",
+                        To_Address   => Address,
+                        To_Name      => Name,
+                        Subject      => "Test email",
+                        Text_Part    => "Test email from Yolk",
+                        SMTP_Server  => My.Config.Get (My.SMTP_Host),
+                        SMTP_Port    => My.Config.Get (My.SMTP_Port),
+                        Charset      => UTF8);
+
+         if Composer.Is_Send (Email) then
+            Insert (T, Assoc ("IS_SEND", True));
+            Insert (T, Assoc ("SMTP_HOST",
+              String'(My.Config.Get (My.SMTP_Host))));
+
+            Trace (Handle  => Info,
+                   Message => "Email sent to " &
+                     Address &
+                     " using " &
+                     String'(My.Config.Get (My.SMTP_Host)));
+         else
+            Insert (T, Assoc ("IS_SEND", False));
+            --  Sending failed.
+
+            Trace (Handle  => Error,
+                   Message => "Email failed to " &
+                     Address &
+                     " using " &
+                     String'(My.Config.Get (My.SMTP_Host)));
+
+            Populate_Form (Address => Address,
+                           Name    => Name,
+                           T                 => T);
+         end if;
+      else
+         Insert (T, Assoc ("IS_SEND", False));
+         --  No recipient address, so obviously we cannot send the email.
+
+         Populate_Form (Address => Address,
+                        Name    => Name,
+                        T                 => T);
+      end if;
+   end Send_Email;
 
 end View.Email;
